5bd03dae05b4f1728a364a3554ab66c92ca2db5d
mkroeders <mkroeders@gmail.com>
2022-03-04 23:05:44 +0100

    Move attribute functions in helper class

---
+++ src/Helper/AttributeHelper.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace LessDocumentor\Helper;
+
+use LessDocumentor\Route\Exception\MissingAttribute;
+use ReflectionAttribute;
+use ReflectionClass;
+
+/**
+ * @internal
+ */
+final class AttributeHelper
+{
+    private function __construct()
+    {}
+
+    /**
+     * @param ReflectionClass<object> $reflector
+     * @param class-string<T> $nameAttribute
+     *
+     * @template T
+     *
+     * @return iterable<T>
+     */
+    public static function getAttributes(ReflectionClass $reflector, string $nameAttribute): iterable
+    {
+        return array_map(
+            static fn (ReflectionAttribute $attribute) => $attribute->newInstance(),
+            $reflector->getAttributes($nameAttribute)
+        );
+    }
+
+    /**
+     * @param ReflectionClass<object> $reflector
+     * @param class-string<T> $nameAttribute
+     *
+     * @template T
+     *
+     * @return T
+     *
+     * @throws MissingAttribute
+     */
+    public static function getAttribute(ReflectionClass $reflector, string $nameAttribute)
+    {
+        foreach ($reflector->getAttributes($nameAttribute) as $attribute) {
+            return $attribute->newInstance();
+        }
+
+        throw new MissingAttribute((string)$reflector, $nameAttribute);
+    }
+
+    /**
+     * @param ReflectionClass<object> $reflector
+     * @param class-string $nameAttribute
+     */
+    public static function hasAttribute(ReflectionClass $reflector, string $nameAttribute): bool
+    {
+        return count($reflector->getAttributes($nameAttribute)) > 0;
+    }
+}
