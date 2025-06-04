7927d1fd559f105d1e2971b35281a2d65486ec88
mkroeders <mkroeders@gmail.com>
2025-06-04 21:10:51 +0200

    Move to union types

--- test/Type/ObjectOutputTypeDocumentorTest.php
+++ test/Type/ObjectOutputTypeDocumentorTest.php
@@ -5,6 +5,9 @@ namespace LesDocumentorTest\Type;
 
 use PHPUnit\Framework\Attributes\CoversClass;
 use LesDocumentor\Type\Document\BoolTypeDocument;
+use LesDocumentor\Type\Document\NullTypeDocument;
+use LesDocumentor\Type\Document\EnumTypeDocument;
+use LesDocumentor\Type\Document\UnionTypeDocument;
 use LesDocumentor\Type\ClassPropertiesTypeDocumentor;
 use LesDocumentor\Type\Document\CompositeTypeDocument;
 use LesValueObject\Number\Int\Paginate\PerPage;
@@ -45,9 +48,12 @@ final class ObjectOutputTypeDocumentorTest extends TestCase
 
         $stub = $document->properties[1];
         self::assertTrue($stub->key->matches('stub'));
-        self::assertSame(['foo', 'fiz'], $stub->type->cases);
-        self::assertSame(EnumStub::class, $stub->type->getReference());
-        self::assertNull($stub->type->getDescription());
+        self::assertInstanceOf(UnionTypeDocument::class, $stub->type);
+        self::assertInstanceOf(NullTypeDocument::class, $stub->type->subTypes[0]);
+        self::assertInstanceOf(EnumTypeDocument::class, $stub->type->subTypes[1]);
+        self::assertSame(['foo', 'fiz'], $stub->type->subTypes[1]->cases);
+        self::assertSame(EnumStub::class, $stub->type->subTypes[1]->getReference());
+        self::assertNull($stub->type->subTypes[1]->getDescription());
 
         $biz = $document->properties[2];
         self::assertTrue($biz->key->matches('biz'));
